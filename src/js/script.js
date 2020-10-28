b706661825be8c6168b8e2d6704a7b85192e9bb6
Lucas Joliveau <joliveau.lucas@gmail.com>
2020-10-28 18:10:23 -0400

    Add animations & skills section

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,46 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    new Swiper(".swiper-container", {
-        pagination: {
-            el: ".swiper-pagination"
-        },
-        autoplay: {
-            delay: 3e3
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 50
-            },
-            1e3: {
-                slidesPerView: 3,
-                spaceBetween: 20
-            }
-        }
-    });
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    // Locomotive Scroll Initalisation
+    (function () {
+        var scroll = new LocomotiveScroll({
+            el: document.querySelector("[data-scroll-container]"),
+            smooth: true,
+            smoothMobile: true,
+            // direction:"horizontal",
+            // inertia:1,
+            // repeat:true,
+            offset: ["10%", 100]
+        });
+    })();
+
+//     document.querySelector('.main-skills').addEventListener('mouseenter', skillsLine1);
+//
+// function skillsLine1 () {
+//         document.querySelector('.skills-1').classList.add('skills-big-title-gradient');
+//         // document.querySelector('.skills-' + i).classList.remove('.skills-big-title-gradient');
+//         setTimeout(skillsLine2, 1500);
+//         console.log('bjr');
+// }
+//
+//     function skillsLine2 () {
+//             document.querySelector('.skills-1').classList.remove('skills-big-title-gradient');
+//             document.querySelector('.skills-2').classList.add('skills-big-title-gradient');
+//             // document.querySelector('.skills-' + i).classList.remove('.skills-big-title-gradient');
+//             setTimeout(skillsLine3, 1500);
+//     }
+//
+//     function skillsLine3 () {
+//             document.querySelector('.skills-2').classList.remove('skills-big-title-gradient');
+//             document.querySelector('.skills-3').classList.add('skills-big-title-gradient');
+//             // document.querySelector('.skills-' + i).classList.remove('.skills-big-title-gradient');
+//             setTimeout( () => {
+//                 document.querySelector('.skills-3').classList.remove('skills-big-title-gradient');
+//                 skillsLine1();
+//             }, 1500);
+//         document.querySelector('.main-skills').removeEventListener('mouseenter', () => {
+//             console.log('bjr');
+//         });
+//
+//     }
+
 });
