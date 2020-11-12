f240a3a136a9213e9845c062b682e6260045f3bb
Lucas Joliveau <joliveau.lucas@gmail.com>
2020-11-11 23:59:06 -0500

    Modify nav bar for all projects. Add used software for case study pages

--- src/js/script.js
+++ src/js/script.js
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
         delay: 0.4,
     });
 
-    var t1 = new TimelineMax({ paused: true });
+    var t1 = new TimelineMax({paused: true});
 
     t1.to(".nav", 1.8, {
         width: "100%",
         ease: Expo.easeInOut,
     });
 
-    t1.staggerTo(".nav-item a", 0.6, { top: "0px", ease: Expo.easeInOut }, 0.1, "-=0.8");
+    t1.staggerTo(".nav-item a", 0.6, {top: "0px", ease: Expo.easeInOut}, 0.1, "-=0.8");
 
     t1.reverse();
     $(document).on("click", ".nav-toggle", function () {
@@ -30,16 +30,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
         document.querySelector('body').classList.remove('overflow-menu');
     });
 
+    // Hide navigation menu when mouse scroll
     var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
+
+    // When scroll is detected
+    window.onscroll = function () {
+        // Get current scroll Y axis position
         var currentScrollPos = window.pageYOffset;
+
+        // If the previous scroll position is higher than the current
         if (prevScrollpos > currentScrollPos) {
+            // Show the navigation bar, so the top position is 0
             document.getElementById("navbarid").style.top = "0";
+
+            // Hide the navigation bar, so the top position is minus 100
         } else {
             document.getElementById("navbarid").style.top = "-100px";
         }
         prevScrollpos = currentScrollPos;
     }
 
-    console.log(prevScrollpos);
+
+
 });
