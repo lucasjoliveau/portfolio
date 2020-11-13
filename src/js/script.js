a99bbda5c1f54f1a128b196c2d83d76db0f2c30d
Lucas Joliveau <joliveau.lucas@gmail.com>
2020-11-13 12:41:35 -0500

    Add favicon and modify about section

--- src/js/script.js
+++ src/js/script.js
@@ -2,55 +2,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // Initialisation of AOS Animate Library
     AOS.init();
 
-    TweenMax.from(".navbar", 2, {
-        top: "-75px",
-        ease: Expo.easeInOut,
-        delay: 0.4,
-    });
-
-    var t1 = new TimelineMax({paused: true});
-
-    t1.to(".nav", 1.8, {
-        width: "100%",
-        ease: Expo.easeInOut,
-    });
-
-    t1.staggerTo(".nav-item a", 0.6, {top: "0px", ease: Expo.easeInOut}, 0.1, "-=0.8");
-
-    t1.reverse();
-    $(document).on("click", ".nav-toggle", function () {
-        t1.reversed(!t1.reversed());
-        document.querySelector('.nav-toggle').classList.toggle('nav-toggle-active');
-        document.querySelector('body').classList.toggle('overflow-menu');
-    });
-
-    $(document).on("click", ".nav-item a", function () {
-        t1.reversed(!t1.reversed());
-        document.querySelector('.nav-toggle').classList.remove('nav-toggle-active');
-        document.querySelector('body').classList.remove('overflow-menu');
-    });
-
-
-    paceOptions = {
-        ajax: true,
-        document: true,
-        eventLag: false
-    };
-
-    Pace.on('done', function() {
-        $('.logo-preloader').delay(500).animate({top: '30%', opacity: '0'}, 3000, $.bez([0.19,1,0.22,1]));
-
-
-        $('#preloader').delay(1500).animate({top: '-100%'}, 2000, $.bez([0.19,1,0.22,1]));
-
-        TweenMax.from(".title", 2, {
-            delay: 1.8,
-            y: 10,
-            opacity: 0,
-            ease: Expo.easeInOut
-        })
-    });
-
     // Detect when view video link is clicked. If, then it open the modal window and remove overflow behavior of body.
     document.querySelector('.modale-ytb-link').addEventListener('click', () => {
         document.querySelector(".modale-ytb").classList.remove('modale-ytb-hidden');
@@ -65,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
 
 });
+
 
 // Load YouTube video
 var tag = document.createElement('script');
@@ -81,6 +33,10 @@ function onYouTubeIframeAPIReady() {
         height: '540',
         width: '960',
         videoId: 'ORNZKKXEkTQ',
+        playerVars: {
+            modestbranding: 1,
+            rel: 0
+        },
         events: {
             'onReady': onPlayerReady
         }
