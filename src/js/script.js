3c49f2463f51a7b4a83d26486c483e7f2cb42dbc
Lucas Joliveau <joliveau.lucas@gmail.com>
2020-11-14 17:46:30 -0500

    Modify YouTube video

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     // Initialisation of AOS Animate Library
     AOS.init();
+
+    if (window.location.pathname === "/index.html") {
+        // Detect when view video link is clicked. If, then it open the modal window and remove overflow behavior of body.
+        document.querySelector('.modale-ytb-link').addEventListener('click', () => {
+            document.querySelector(".modale-ytb").classList.remove('modale-ytb-hidden');
+            document.querySelector("body").classList.add('overflow-menu');
+        });
+
+        // Detect when the modal window is clicked to close the window and stop the video.
+        document.querySelector('.modale-ytb').addEventListener('click', () => {
+            document.querySelector("body").classList.remove('overflow-menu');
+            document.querySelector(".modale-ytb").classList.add('modale-ytb-hidden');
+            stopVideo();
+        });
+    }
 });
-
-if (window.location.pathname === "/index.html") {
-    // Detect when view video link is clicked. If, then it open the modal window and remove overflow behavior of body.
-    document.querySelector('.modale-ytb-link').addEventListener('click', () => {
-        document.querySelector(".modale-ytb").classList.remove('modale-ytb-hidden');
-        document.querySelector("body").classList.add('overflow-menu');
-    });
-
-    // Detect when the modal window is clicked to close the window and stop the video.
-    document.querySelector('.modale-ytb').addEventListener('click', () => {
-        document.querySelector("body").classList.remove('overflow-menu');
-        document.querySelector(".modale-ytb").classList.add('modale-ytb-hidden');
-        stopVideo();
-    });
-
 
 // Load YouTube video
     var tag = document.createElement('script');
@@ -33,7 +33,7 @@ if (window.location.pathname === "/index.html") {
         player = new YT.Player('player', {
             height: '540',
             width: '960',
-            videoId: 'ORNZKKXEkTQ',
+            videoId: 'Uhlv_DBnZ0s',
             playerVars: {
                 modestbranding: 1,
                 rel: 0
@@ -55,4 +55,3 @@ if (window.location.pathname === "/index.html") {
         player.stopVideo();
     }
 
-}
