516c6cc01eb06dc50135e7703d4207d62fe48791
Lucas Joliveau <joliveau.lucas@gmail.com>
2020-11-15 12:29:36 -0500

    Make files to php and modify header section

---
+++ src/contactform.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_POST['submit'])) {
+
+    //Creating variables
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $company = $_POST['company'];
+    $emailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    date_default_timezone_set('America/Toronto');
+    $date = date('d-m-y');
+    $hour = date('h:i:s');
+
+    $mailTo = "lucas@joliveau.com";
+    $headers = $firstName . " " . $lastName . " vous à envoyé une demande de contact le " . $date . " à " . $hour . "." . "\n\n" . "Voici les informations détaillées sur le contact: " . "\n" . "Prénom: " . $firstName . "\n" . "Nom de famille: " . $lastName . "\n" . "Entreprise/Organisation: " . $company . "\n" . "Adresse courriel: " . $emailFrom . "\n" . "Objet: " . $subject . "\n\n" . "N'oubliez pas que vous avez assuré à la personne une réponse sous 24 heures.";
+    $txt = "[STAGE] " . $firstName . "\n" . $lastName . " chez " . $company . " est intéressé par votre candidature.";
+
+    mail($mailTo, $txt, $headers);
+    header("Location: formtarget?mailsend");
+}
---
+++ src/formtarget.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lucas Joliveau | Confirmation d'envoi</title>
+    <meta name="apple-mobile-web-app-title" content="Lucas Joliveau">
+    <meta name="application-name" content="Lucas Joliveau">
+    <meta name="msapplication-TileColor" content="#081420">
+    <meta name="theme-color" content="#081420">
+    <meta name="author" content="Lucas Joliveau">
+    <meta name="description"
+          content="En recherche de stage en design UX/UI et en développement front-end en région de Montréal. Consultez mon portfolio dès maintenant.">
+    <meta name="keywords"
+          content="designer, longueuil, design, développement web, portfolio, stagiaire, hiver, montréal, canada, 2021">
+    <meta name="og:locale" content="fr_CA">
+    <meta name="og:type" content="website">
+    <meta name="og:title" content="Lucas Joliveau">
+    <meta name="og:description"
+          content="En recherche de stage en design UX/UI et en développement front-end en région de Montréal. Consultez mon portfolio dès maintenant.">
+    <meta name="og:url" content="https://lucasjoliveau.com">
+    <meta name="og:site_name" content="Lucas Joliveau">
+    <meta property="article:modified_time" content="2020-11-15:46:56+00:00" />
+    <meta property="og:image" content="images/thumbnail.jpg" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="900" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Lucas Joliveau" />
+    <meta name="twitter:description" content="En recherche de stage en design UX/UI et en développement front-end en région de Montréal. Consultez mon portfolio dès maintenant." />
+    <meta name="twitter:image" content="images/thumbnail.jpg" />
+    <meta name="twitter:label1" content="Written by">
+    <meta name="twitter:data1" content="Lucas Joliveau">
+    <meta name="twitter:label2" content="Est. reading time">
+    <meta name="twitter:data2" content="3 minute">
+    <link rel="stylesheet" href="css/aos.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/pace.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-bez@1.0.11/src/jquery.bez.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min.js" defer></script>
+    <script src="js/aos.js" defer></script>
+    <script src="js/script.js"></script>
+    <script src="js/jquery.js" defer></script>
+    <script src="js/lazysizes.min.js" defer></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script src="https://www.googletagmanager.com/gtag/js?id=G-0Z84MLQ511" defer></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'G-0Z84MLQ511');
+    </script>
+</head>
+<body>
+<header>
+    <nav>
+        <div class="navbar">
+            <div class="wrapper wrap">
+                <a class="nav-logo" href="index">Lucas Joliveau</a>
+                <div class="nav-toggle">
+                    <span class="nav-line nav-line-top"></span>
+                    <span class="nav-line nav-line-bottom"></span>
+                </div>
+            </div>
+        </div>
+        <div class="wrapper wrap">
+            <div class="nav">
+                <div class="nav-items wrapper wrap">
+                    <div class="nav-item">
+                        <a href="index#case-studies">Études de cas</a>
+                        <div class="nav-item-wrapper"></div>
+                    </div>
+                    <div class="nav-item">
+                        <a href="index#about">À propos</a>
+                        <div class="nav-item-wrapper"></div>
+                    </div>
+                    <div class="nav-item">
+                        <a href="index#contact">Contact</a>
+                        <div class="nav-item-wrapper"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+<div id="preloader">
+    <img class="logo-preloader" src="images/logo.svg" alt="Logo Lucas Joliveau">
+</div>
+
+<!--HERO SECTION-->
+<section class="hero hero-global">
+    <div class="wrapper wrap">
+        <h1>Votre demande de contact a été envoyée avec succès.</h1>
+        <p>Vous recevrez une réponse à votre demande dans un délai maximum de 24 heures.</p>
+        <div data-aos="fade-up" class="button button-alt"><a href="index">Retourner à l'accueil</a></div>
+    </div>
+</section>
+
+<?php
+include('footer.php');
+?>
